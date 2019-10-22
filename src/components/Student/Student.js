657413c8d8856f533a688effcf492db68a6ce3f9
Shokupanman <Harrisonkaidehancock@gmail.com>
2019-10-22 16:44:48 -0600

    berryyy gguuuud

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Student extends Component {
   constructor() {
-    super()
+    super();
+    this.state = {
+      studentInfo: {}
+    };
+  }
 
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(results => {
+        this.setState({
+          student: results.data
+        });
+      });
   }
 
   render() {
     return (
       <div className="box">
         <h1>Student</h1>
+        <h2>
+          {this.state.studentInfo.first_name} {this.state.studentInfo}
+        </h2>
+
+        <h3>Grade:</h3>
+        <h4>{this.state.studentInfo.grade}</h4>
+
+        <h3>Email:</h3>
+        <h4>{this.state.studentInfo.email}</h4>
       </div>
-    )
+    );
   }
 }
